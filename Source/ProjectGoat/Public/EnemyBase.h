f3db075a59bbcf6ba0d6ebea8c5572e57679d2ff
czhou2822 <zhouck0811@gmail.com>
2020-04-16 17:43:48 -0400

    AlphaMap added.
    Slow enemy added.

---
+++ Source/ProjectGoat/Public/EnemyBase.h
@@ -0,0 +1,50 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "EnemyBase.generated.h"
+
+UCLASS()
+class PROJECTGOAT_API AEnemyBase : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AEnemyBase();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditDefaultsOnly)
+	float DefaultMaxSpeed = 600.f;
+
+	UPROPERTY(EditDefaultsOnly)
+	float SlowDownPercentage = 0.5f;
+
+	FTimerHandle SlowTimer;
+
+	float TimerTickInterval = 0.1f;
+
+	UPROPERTY(EditDefaultsOnly)
+	float SlowedTime = 2.f;
+
+	int32 TickCount = 0;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	UFUNCTION(BlueprintCallable)
+	void SlowDown();
+
+	UFUNCTION()
+	void HandleSlowDown();
+
+};
