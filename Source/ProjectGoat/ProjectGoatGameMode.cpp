5abb36c93354690c70b30c630ec4bf5244683ed4
czhou2822 <zhouck0811@gmail.com>
2020-04-19 15:01:53 -0400

    Tower Spawn added.

--- Source/ProjectGoat/ProjectGoatGameMode.cpp
+++ Source/ProjectGoat/ProjectGoatGameMode.cpp
@@ -7,9 +7,9 @@
 AProjectGoatGameMode::AProjectGoatGameMode()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
-	if (PlayerPawnBPClass.Class != NULL)
-	{
-		DefaultPawnClass = PlayerPawnBPClass.Class;
-	}
+	//static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
+	//if (PlayerPawnBPClass.Class != NULL)
+	//{
+	//	DefaultPawnClass = PlayerPawnBPClass.Class;
+	//}
 }
