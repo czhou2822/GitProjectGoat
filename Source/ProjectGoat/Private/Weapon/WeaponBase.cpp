256e09711d96428d1913d122f765c28286946071
czhou2822 <zhouck0811@gmail.com>
2020-05-08 09:09:56 -0400

    Frost Cannon added to solider

--- Source/ProjectGoat/Private/Weapon/WeaponBase.cpp
+++ Source/ProjectGoat/Private/Weapon/WeaponBase.cpp
@@ -9,7 +9,7 @@ AWeaponBase::AWeaponBase()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-	WeaponMesh = CreateDefaultSubobject<USkeletalMeshComponent>("WeaponMesh");
+	WeaponMesh = CreateDefaultSubobject<UStaticMeshComponent>("WeaponMesh");
 	RootComponent = WeaponMesh;
 
 	//WeaponCollisionRange = CreateDefaultSubobject<USceneComponent>(TEXT("WeaponCollision"));
