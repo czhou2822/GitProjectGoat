5ac319415d8ff8a206c722b73da7704580055a04
czhou2822 <zhouck0811@gmail.com>
2020-05-30 09:58:11 -0400

    architectural mindmap  added.

--- Source/ProjectGoat/ProjectGoat.Build.cs
+++ Source/ProjectGoat/ProjectGoat.Build.cs
@@ -8,6 +8,12 @@ public class ProjectGoat : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay" });
+		PublicDependencyModuleNames.AddRange(new string[] { 
+			"Core", 
+			"CoreUObject", 
+			"Engine", 
+			"InputCore", 
+			"HeadMountedDisplay" 
+		});
 	}
 }
